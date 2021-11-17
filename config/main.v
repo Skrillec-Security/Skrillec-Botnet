c3be585278e684a588b198a55f0df7e0b13b113d
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-17 13:45:53 -0500

    Create main.v

---
+++ config/main.v
@@ -0,0 +1,8 @@
+module config
+
+pub struct AppInfo{
+	pub mut:
+		title		string = "Skrillec Botnet"
+		description	string = "The New 2022 All-In-One Botnet. Eliminating the hassle of looking for all botnet related files to start one up!"
+		version		string = "4.00"
+}
