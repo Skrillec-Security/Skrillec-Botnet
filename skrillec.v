32c5947a1d0394ea988dd65008fdee4db722aee4
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-17 13:46:08 -0500

    Update skrillec.v

--- skrillec.v
+++ skrillec.v
@@ -18,8 +18,23 @@ import os
 import net
 
 //Importing SubModules
-import Server
+import server
+import config
 
 fn main() {
-	println("  ")
+	mut svr := server.Server{}
+	
+	// Command Parsing 
+	mut cmd_args := os.args.clone()
+	for i, v in cmd_args {
+		if v == "-p" { svr.port = cmd_args[i+1] }
+
+		if v == "reset_config" {
+			// Execute function that'd reset the whole config file to default
+		}
+	}
+	if svr.port.len == 0 {
+		print("[x] Warning. No port was provided. Looking through config file for port.........!")
+		// Execute function to look for PORT in config file
+	}
 }
