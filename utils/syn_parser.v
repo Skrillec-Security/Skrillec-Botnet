e81b09f8b39176df53004109020b889a195712ac
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-19 18:42:44 -0500

    Update syn_parser.v

--- utils/syn_parser.v
+++ utils/syn_parser.v
@@ -3,7 +3,17 @@ module utils
 import os
 import utils
 
+pub struct Parse {
+	pub mut:
+		file			string
+		parsed_data		string	
+		parsed_data_arr	[]string
+
+}
+
 /* 
+			QUICK FUNCTION TO USE WITHOUT STRUCT
+____________________________________________________________________________________________________
 	method =>				parse(data string, field string, key string) (int, string)
 	return type =>			int, string
 	return type notes =>	int is for checking if key's value was found (0 not found / 1 found)
@@ -37,43 +47,12 @@ pub fn parse(data string, field string, key string) (int, string) {
 	return 0, ""
 }
 
-pub fn store_fields_and_keys(data string, field string) (int, [][]string) {
-	mut keys_found := [][]string
-	if data.len == 0 { return 0, keys_found }
-	mut lines := data.split("\n")
-	mut line_count := 0
-	for line in lines {
-		println(line)
-		if line.contains("\"") && line.contains("{") {
-			if utils.trim_all_spaces(line).contains("\"${field}\"{") {
-				mut found := 0
-				mut find_c := line_count
-				for {
-					println(lines[find_c])
-					if utils.trim_all_spaces(lines[find_c]).contains("{") || utils.trim_all_spaces(lines[find_c]).contains("}") { continue }
-					if utils.trim_all_spaces(lines[find_c]) != "}" {
-						mut split_line := lines[find_c].split("=")
-						mut key_check := utils.trim_all_spaces(split_line[0])
-						mut value_check := utils.trim_all_spaces(split_line[1]) // this is the value if it doesnt contain quotes
-						if value_check.contains("\"") && value_check.ends_with("\"") {
-							mut new_value := utils.get_str_between(value_check, "\"", "\"")
-							keys_found << ([key_check, value_check])
-						} else { 
-							keys_found << ([key_check, value_check])
-						}
-					} else {
-						return 1, keys_found
-					}
-					find_c += 1
-				}
-			}
-		}
-		line_count += 1
-	}
-	return 0, keys_found
-}
+// pub fn store_fields_and_keys(data string, field string) (int, [][]string) {
+// 	mut keys_in_fields := [][]string
 
-pub fn (mut s Syn) trim_all_spaces(str string) string {
+// }
+
+pub fn trim_all_spaces(str string) string {
 	if str.len == 0 { return str }
 	mut trimmed := ""
 	mut found := 0
