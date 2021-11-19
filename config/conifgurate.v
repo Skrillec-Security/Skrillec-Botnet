ca5ba761d8de743a62a3885bccc3e14b1d1057bb
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-19 16:49:40 -0500

    Update conifgurate.v

--- config/conifgurate.v
+++ config/conifgurate.v
@@ -1,7 +1,21 @@
 module config
 
 import os
+import utils
 
 pub fn get_net_info() []string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
+	e_code, host := utils.parse(file_d, "MySQL", "host")
+	_, port := utils.parse(file_d, "MySQL", "port")
+	_, username := utils.parse(file_d, "MySQL", "username")
+	_, password := utils.parse(file_d, "MySQL", "password")
+	_, dbname := utils.parse(file_d, "MySQL", "dbname")
+
+	mut reply := []string
+	reply << port
+	reply << username
+	reply << password
+	reply << dbname
+
+	return reply
 }
