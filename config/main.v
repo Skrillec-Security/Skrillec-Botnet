9c1d5d9c964b6912e8ee155918ba3684c9899e13
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-21 13:50:24 -0500

    Update main.v

--- config/main.v
+++ config/main.v
@@ -6,3 +6,33 @@ pub struct AppInfo{
 		description	string = "The New 2022 All-In-One Botnet. Eliminating the hassle of looking for all botnet related files to start one up!"
 		version		string = "4.00"
 }
+
+const (
+	//Colors
+	Default			= ""
+	Black			= ""
+	Red				= ""
+	Green			= ""
+	Yellow			= ""
+	Blue			= ""
+	Purple			= ""
+	Cyan			= ""
+	Light_Grey		= ""
+	Dark_Grey		= ""
+	Light_red		= ""
+	Light_Green		= ""
+	Light_Yellow	= ""
+	Light_Blue		= ""
+	Light_Purple	= ""
+	Light_Cyan		= ""
+	White			= ""
+	// Background Colors
+	Default_BG		= ""
+	Black_BG		= ""
+	Red_BG			= ""
+	Green_BG		= ""
+	Yellow_BG		= ""
+	Blue_BG			= ""
+	Purple_BG		= ""
+	Cyan_BG			= ""
+)
