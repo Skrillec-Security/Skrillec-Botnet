eda5b3b04ba94f80a26c9ac61192a824975bd822
Jebize <73313376+jazuki1337@users.noreply.github.com>
2021-11-21 10:07:55 -0500

    commands
    sss

--- skrillec_cp/main.v
+++ skrillec_cp/main.v
@@ -5,6 +5,9 @@ import os
 pub fn main_cp() {
 	for {
 		mut input_cmd := os.input(">>> ")
-		
+		if input_cmd == "help" {
+		   help := os.read_file('/root/Skrillec-Botnet/banner/7help.txt')
+		   println(help)
+		}
 	}
 }
