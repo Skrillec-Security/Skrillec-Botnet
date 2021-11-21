bd788fb9b5bc80f779df8554b5d00bf69da088c7
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-21 13:50:27 -0500

    Update mysql_utils.v

--- crud/mysql_utils.v
+++ crud/mysql_utils.v
@@ -5,7 +5,19 @@ module crud
 
 import mysql
 
-pub fn get_row(mut s mysql.Connection, q string) string {
-	s.connect() ? or { panic("[x] Error, Failed to connect to MySQL!") }
+pub fn get_row(mut s mysql.Connection, q string) []string {
+	s.connect() or { panic("[x] Error, Failed to connect to MySQL!") exit(0) }
 	q_resp := s.query(q)
+	mut row := []string
+	for i in q_resp.map() {
+		row << i
+	}
+	q_resp.free()
+	s.close()
+	return row
+}
+
+pub fn get_all_rows(mut s mysql.Connection, table string) {
+	s.connect() or { panic("[x] Error, Failed to connect to MySQL!") exit(0)}
+	table_query := s.query()
 }
