36ef5fdb8381de6a38961f75a79894013f7fbeca
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-21 18:12:13 -0500

    Update conifgurate.v

--- config/conifgurate.v
+++ config/conifgurate.v
@@ -5,6 +5,24 @@ import utils
 
 pub fn get_net_info() []string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
+	mut info := []string
+	e_code, net_title := utils.parse(file_d, "Net_Info", "net_title")
+	_, net_description := utils.parse(file_d, "Net_Info", "net_description")
+	_, net_versoin := utils.parse(file_d, "Net_Info", "net_versoin")
+	_, net_port := utils.parse(file_d, "Net_Info", "net_port")
+	_, skrillec_key := utils.parse(file_d, "Net_Info", "skrillec_key")
+	
+	info << net_title
+	info << net_description
+	info << net_versoin
+	info << net_port
+	info << skrillec_key
+
+	return info
+}
+
+pub fn get_db_info() []string {
+	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
 	e_code, host := utils.parse(file_d, "MySQL", "host")
 	_, port := utils.parse(file_d, "MySQL", "port")
 	_, username := utils.parse(file_d, "MySQL", "username")
@@ -18,15 +36,4 @@ pub fn get_net_info() []string {
 	reply << dbname
 
 	return reply
-}
-
-pub fn get_db_info() []string {
-	//detect for 'localfiles' structure or 'MySQL' structure in the config file
-	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
-	if file_d.contains("MySQL") { // first choice to look for before local db files
-		//get all keys and return em 
-		mut keys := []string
-	}
-	return ["", ""]
-	// this function isnt finished!!
 }
