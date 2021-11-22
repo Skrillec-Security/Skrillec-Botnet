c9088d3acdf285b9c6c9939a86bda90e52ea65fc
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-22 11:08:21 -0500

    Update api_attack.v

--- attack_system/api_attack.v
+++ attack_system/api_attack.v
@@ -1,6 +1,14 @@
 module attack_system
 
 
-pub fn send_attack(ip string, port string, time string, method string) {
+pub fn send_attack(ip string, port string, time string, method string) int {
+
+}
+
+pub fn fill_api(ip string, port string, time string, method string) {
+
+}
+
+pub fn get_all_api() {
 	
 }
