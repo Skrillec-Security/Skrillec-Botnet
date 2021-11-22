7eb335b8f74dd4232bfe130ba0f986026d34880a
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-22 14:32:17 -0500

    Update notifications.v

--- utils/notifications.v
+++ utils/notifications.v
@@ -1,12 +1,38 @@
 module utils
 
 import os
+import net
 
-pub fn notificate(user string, cmd string, text string, mut s server.Server) {
+pub struct NotificationSys {
+	pub mut:
+		user			string
+		ip				string
+		cmd				string
+		fmsg			string
+		notificate_msg	string
+}
+
+pub fn (mut n NotificationSys) set_info(user string, ip string, cmd string, fmsg string, notificate_msg string) {
+	n.user = user
+	n.ip = ip 
+	n.cmd = cmd
+	n.fmsg = fmsg
+	n.notificate_msg = notificate_msg
+}
+
+pub fn (mut n NotificationSys) notificate() {
 	//get the current time
 	mut current_time := os.execute("date +'%m/%d/%y-%H:%m'").output
-	mut current_userip := s.clients.get_ip(user)
-	mut output := "[Action/CMD]: ${cmd}| [Timestamp]: ${current_time}\r\n"
-	output += "[User]: ${user} | [IP]: ${current_userip}\r\n"
-	output += "[Message]: ${s.File}"
+	mut output := "[Action/CMD]: ${n.cmd}| [Timestamp]: ${current_time}\r\n"
+	output += "[User]: ${n.user} | [IP]: \r\n"
+	// output += "[Message]: ${s.File}"
+	n.reset_info()
+}
+
+pub fn (mut n NotificationSys) reset_info() {
+	n.user = ""
+	n.ip = "" 
+	n.cmd = ""
+	n.fmsg = ""
+	n.notificate_msg = ""
 }
