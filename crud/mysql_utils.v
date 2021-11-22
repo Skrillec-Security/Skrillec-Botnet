da16cc9dd7e39a9444eebcff30fbc96edb591ba9
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-22 11:09:57 -0500

    Update mysql_utils.v

--- crud/mysql_utils.v
+++ crud/mysql_utils.v
@@ -5,17 +5,29 @@ module crud
 
 import mysql
 
-pub fn get_row(mut s mysql.Connection, q string) []string {
-	s.connect() or { panic("[x] Error, Failed to connect to MySQL!") exit(0) }
-	q_resp := s.query(q)
-	mut row := []string
-	for i in q_resp.map() {
-		row << i
-	}
-	q_resp.free()
-	s.close()
-	return row
+/* This function reads all users from mySQL DB */
+fn grab_users(mut s mysql.Connection, user string) []string {
+        s.connect() or { panic("[x] Error, Failed to connect to MySQL!") }
+        q_resp := s.query(q) or { panic("Unable to send query to MySQL!") }
+        mut row := []string
+        for i in q_resp.maps() {
+			if i['username'] == user {
+                row << i['username']
+                row << i['ip']
+                row << i['password']
+                row << i['level']
+                row << i['maxtime']
+                row << i['conn']
+                row << i['ongoing']
+                row << i['admin']
+                row << i['expiry']
+			}
+        }
+        q_resp.free()
+        s.close()
+        return row
 }
+
 
 /*
 get_all_rows(mysql.Connection{
@@ -26,7 +38,7 @@ get_all_rows(mysql.Connection{
 	dbname: ""
 }, "ongoing", "users", "username=root") // this function called here is pulling 'ongoing' value from the username root in MySQL db
 */
-pub fn get_all_rows(mut s mysql.Connection, selectt string, from string, where string) {
+pub fn mysql_select(mut s mysql.Connection, selectt string, from string, where string) {
 	s.connect() or { panic("[x] Error, Failed to connect to MySQL!") exit(0)}
 	table_query := s.query('SELECT * FROM ${table}')
 	if where.len > 0 {
