5d3e58bfc03aee1f3371062fa373185eb80081ee
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-22 14:31:48 -0500

    Update banner_utils.v

--- utils/banner_utils.v
+++ utils/banner_utils.v
@@ -1,1 +1,21 @@
 module utils
+
+import os
+import net
+import utils
+
+pub struct CurrentLine {
+	pub mut:
+		line	int
+}
+
+pub fn output_banner(mut socket net.TcpConn) {
+	mut banner_file := os.read_file(os.getwd() + "/banners/${file}.txt") or { "" }
+	mut config_file := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
+	_, output_position := utils.parse(config_file, "CNC_Output", "cnc_output_position")
+	if banner_file.contains("\n") {
+		for i in banner_file.split("\n") {
+			
+		}
+	}
+}
