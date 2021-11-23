27720474a2a5734fafee94471fccfb950e7b8ae4
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-23 11:21:44 -0500

    Update login.v

--- auth/login.v
+++ auth/login.v
@@ -1,5 +1,9 @@
 module auth
 
+import net
+import crud
+import mysql
+
 pub struct CurrentLogin{
 	pub mut:
 		username 	string
@@ -7,6 +11,28 @@ pub struct CurrentLogin{
 		ip			string
 }
 
-pub fn login(mut c CurrentLogin) {
-	
+pub fn login(mut sql mysql.Connection, mut socket net.TcpConn, c CurrentLogin) int {
+	print("here")
+	get_info := crud.get_user(mut sql, c.username)
+	if get_info.len == 0 {
+		socket.write_string("[x] Error, Invalid information provided!....\r\nClosing app....!\r\n") or { continue }
+		exit(0)
+	}
+
+	if get_info[0] == c.username.str() && get_info[2] == c.password {
+		if get_info[1] == c.ip {
+			return 1
+			socket.write_string("[x] Welcome ${c.username} to Skrillec NET!\r\n") or { 0 }
+		} else {
+			return 0
+			socket.write_string("[x] Error, IP Lock does not match!....\r\n[x] Closing app....\r\n") or { continue }
+			exit(0)
+		}
+	} else {
+		return 0
+		socket.write_string("[x] Error, Invalid information provided!....\r\nClosing app....!\r\n") or { continue }
+		exit(0)
+	}
+
+	return 0
 }
