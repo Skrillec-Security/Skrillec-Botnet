7619b469f31dbc7363299706dd5147454a6a529a
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-24 13:39:56 -0500

    Update login.v

--- auth/login.v
+++ auth/login.v
@@ -5,6 +5,7 @@ import time
 import auth
 import crud
 import mysql
+import config
 import crypto.sha1
 
 pub struct CurrentLogin{
@@ -18,24 +19,25 @@ pub fn login(mut sql mysql.Connection, mut socket net.TcpConn, c CurrentLogin) i
 	get_info := crud.get_user(mut sql, c.username)
 	
 	if get_info.len == 0 {
-		socket.write_string("[x] Error, Invalid information provided!....\r\nClosing app....!\r\n") or { 0 }
-		time.sleep(2 * time.second)
+		socket.write_string("${config.Red}[x] Error, Invalid information provided!....\r\nClosing app....!${config.Default}\r\n") or { 0 }
+		time.sleep(4 * time.second)
 		socket.close() or { return 0 }
+		return 0
 	}
 
 	if get_info[0] == c.username.str().replace("\r\n", "") && get_info[2] == sha1.hexhash(c.password.replace("\r\n", "")) {
 		if get_info[1] == c.ip || c.ip == "none" || c.username.str().replace("\r\n", "") == "root" {
-			socket.write_string("[x] Welcome ${c.username} to Skrillec NET!\r\n") or { 0 }
+			socket.write_string("${config.Green}[x] Welcome ${c.username} to Skrillec NET!${config.Green}\r\n") or { 0 }
 			return 1
 		} else {
-			socket.write_string("[x] Error, IP Lock does not match!....\r\n[x] Closing app....\r\n") or { 0 }
-			time.sleep(2 * time.second)
+			socket.write_string("${config.Red}[x] Error, IP Lock does not match!....\r\n[x] Closing app....${config.Default}\r\n") or { 0 }
+			time.sleep(4 * time.second)
 			socket.close() or { return 0 }
 			return 0
 		}
 	} else {
-		socket.write_string("[x] Error, Invalid information provided!....\r\nClosing app....!\r\n") or { 0 }
-		time.sleep(2 * time.second)
+		socket.write_string("${config.Red}[x] Error, Invalid information provided!....\r\nClosing app....!${config.Default}\r\n") or { 0 }
+		time.sleep(4 * time.second)
 		socket.close() or { return 0 }
 		return 0
 	}
