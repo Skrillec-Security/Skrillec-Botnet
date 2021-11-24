52093bbba779a9d3edc19e0f2f2e778bebeff3b7
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-24 14:51:04 -0500

    Update custom_net_functions.v

--- utils/custom_net_functions.v
+++ utils/custom_net_functions.v
@@ -19,3 +19,7 @@ pub fn place_text_from_file(mut socket net.TcpConn, r int, c int, file string) {
 	mut file_d := os.read_file(os.getwd() + "/banners/${file}_text.txt") or { "" }
 	socket.write_string("\033[${r};${c}f${file_d}") or { 0 }
 }
+
+pub fn move_cursor(mut socket net.TcpConn, r int, c int) {
+	socket.write_string("\033[${r};${c}f") or { 0 }
+}
