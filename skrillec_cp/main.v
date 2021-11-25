0b61eec72a06235a378691093eb1aecaa8ecb989
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-25 16:15:14 -0500

    Update main.v

--- skrillec_cp/main.v
+++ skrillec_cp/main.v
@@ -1,10 +1,28 @@
 module skrillec_cp
 
 import os
+import net
+import server
 
-pub fn main_cp() {
+pub fn main_cp(mut svr server.Server) {
 	for {
 		mut input_cmd := os.input(">>> ")
-		
+		mut input_args := []string
+		if input_cmd.contains(" ") { input_args = input_cmd.split(" ") }
+
+		if input_cmd == "onlines" {
+			println("Online Users")
+			for i, username in svr.clients.u_name {
+				println("${i} | ${username} | ${svr.clients.u_ip[i]}")
+			}
+		} else if input_cmd.starts_with("boardcast") {
+			if input_args < 2 {
+				full_msg := input_cmd.replace("${input_args} ", "")
+				for i in svr.clients.u_sockets {
+					i.write_string(full_msg) or { 0 }
+				}
+				println("Message sent to all users")
+			}
+		}
 	}
 }
