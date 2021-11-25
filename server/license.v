042aa026a86f02cb38702052fb322939ac40ec8c
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-24 19:47:08 -0500

    Update license.v

--- server/license.v
+++ server/license.v
@@ -3,6 +3,19 @@ module server
 import net
 import net.http
 
-pub fn validate_token(token string) {
+pub fn update_check() {
+	
+}
 
+pub fn validate_token(token string) {
+	mut try_key := http.get_text("https://codetheworld.xyz/skrillec/?key=${token}")
+	if try_key.starts_with("[x]") {
+		print("[x] Error, Invalid key!\r\n")
+		exit(0)
+	} else if try_key.starts_with("[+]") {
+		print("[+] Key Authorized! Welcome to Skrillec NET v4.0.0\r\n")
+	} else {
+		print("Something went wrong trying to validate your skrillec token! please try again later or contact owner!\r\n")
+		exit(0)
+	}
 }
