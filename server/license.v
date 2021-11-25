8705cbbcfa84cf2abb11b745d4129d3330598890
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-25 12:46:01 -0500

    Update license.v

--- server/license.v
+++ server/license.v
@@ -2,20 +2,23 @@ module server
 
 import net
 import net.http
+import server
 
 pub fn update_check() {
-	
+	mut check_update := http.get_text("https://codetheworld.xyz/skrillec/?key=gfrwegregregergergeg")
+	if check_update.contains("Update: 4.0.0") {} else { print("[x] A new update was found. Download the new update! Auto update is coming soon!\r\n") exit(0) }
 }
 
-pub fn validate_token(token string) {
-	mut try_key := http.get_text("https://codetheworld.xyz/skrillec/?key=${token}")
-	if try_key.starts_with("[x]") {
+pub fn validate_token(mut svr server.Server) int {
+	mut try_key := http.get_text("https://codetheworld.xyz/skrillec/?key=${svr.cnc_key}")
+	if try_key.contains("[x]") {
 		print("[x] Error, Invalid key!\r\n")
-		exit(0)
-	} else if try_key.starts_with("[+]") {
+		return 0
+	} else if try_key.contains("[+]") {
 		print("[+] Key Authorized! Welcome to Skrillec NET v4.0.0\r\n")
+		return 1
 	} else {
 		print("Something went wrong trying to validate your skrillec token! please try again later or contact owner!\r\n")
-		exit(0)
+		return 0
 	}
 }
