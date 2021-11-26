bd9a6e9aeee4329431608975b065c85a779733d5
Jebize <73313376+jazuki1337@users.noreply.github.com>
2021-11-26 03:38:25 +0100

    Update cnc.v
    added captcha

--- server/cnc.v
+++ server/cnc.v
@@ -48,6 +48,28 @@ pub fn (mut s Server) connection_handler(mut socket net.TcpConn) {
 	utils.change_size(mut socket, 22, 106)
 	print("New User Connected!: ${fixed_ip}:${fixed_port}\r\n")
 	mut start_current := server.Current{}
+	//Add captcha here to avoid bot logins or other things
+	/*
+		Captcha shit here
+	*/
+	num := rand.int_in_range(1, 1000)
+    lol := http.get_text("https://artii.herokuapp.com/make?text=$num&font=italic").split("\n")
+    mut output := "\r" 
+    for i in lol {
+    output += "\r$i\n" 
+    }
+    socket.write_string(output) or {0}
+    socket.write_string("\r\n >>> [38;2;60;195;255m[38;2;55;200;255m[38;2;50;205;255m[38;2;45;210;255m[38;2;40;215;255m[38;2;35;220;255m") or {0}
+    data := reader.read_line() or {return}
+    if data.contains('$num'){
+    conn.write_string("Captcha completed!\r\n") or {0}
+    println('someone completed the captcha') 
+    }
+	else
+	{
+    conn.write_string("Invalid captcha!") or {0}
+    println('someone failed the captcha')
+    }
 	//Add login here then log the user's username, IP, and detect if user is using the Skrillec CLIENT to connect
 	/*
 		Login shit here
