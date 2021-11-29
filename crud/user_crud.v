576b738fd61373f585bdcbebb367f1f1501c5028
social reject <94141577+9xN@users.noreply.github.com>
2021-11-29 17:38:45 +0000

    woah there buckaroo (maybe try to test b4 push)

--- crud/user_crud.v
+++ crud/user_crud.v
@@ -1,21 +1,9 @@
-module crud
+module crud 
 
 import crud
 import mysql
 
-pub fn get_user(username string, mut s mysql.Connection) {
-        rows := s.query("select $username from users") ?
-        return rows
-}
-
-pub fn create_user(username string, password string, maxtime int, level string, expiry string, admin int, mut s mysql.Connection) {
-        s.insert(table, username, password, maxtime, level, expiry, admin) ?
-}
-
-pub fn delete_user(username string, mut s mysql.Connection) {
-        s.delete(table, username) ?
-        }
-
-pub fn update_user(username string, password string, maxtime int, level string, expiry string, admin int, mut s mysql.Connection) {
-        s.update(table, username, "username")
-}
+pub fn get_user(mut s mysql.Connection, username string) []string {
+	rows := crud.grab_user_info(mut s, username)
+	return rows
+} 
