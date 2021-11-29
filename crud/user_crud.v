d4d26078525afa1bb91754dbeb0a62d5e153a4e1
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-29 12:41:27 -0500

    Update user_crud.v

--- crud/user_crud.v
+++ crud/user_crud.v
@@ -6,8 +6,4 @@ import mysql
 pub fn get_user(mut s mysql.Connection, username string) []string {
 	rows := crud.grab_user_info(mut s, username)
 	return rows
-<<<<<<< Updated upstream
-} 
-=======
 }
->>>>>>> Stashed changes
