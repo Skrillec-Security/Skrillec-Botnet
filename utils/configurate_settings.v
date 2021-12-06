31b9d2877e904d90c4844b02d3672b822102c006
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-06 13:48:09 -0500

    Update configurate_settings.v

--- utils/configurate_settings.v
+++ utils/configurate_settings.v
@@ -5,13 +5,13 @@ import utils
 
 pub fn grab_port() string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
-	_, p := utils.parse(file_d, "Net_Info", "net_port")
+	p := utils.parse(file_d, "Net_Info", "net_port")
 	return p
 }
 
 pub fn grab_cnc_token() string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
 	if file_d.contains("skrillec_key") {} else { return "" }
-	_, token := utils.parse(file_d, "Net_Info", "skrillec_key")
+	token := utils.parse(file_d, "Net_Info", "skrillec_key")
 	return token
 }
