23822a4bab30a565ceb914e25d51b3cbb7c19b19
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-06 13:47:21 -0500

    Update conifgurate.v

--- config/conifgurate.v
+++ config/conifgurate.v
@@ -6,11 +6,11 @@ import utils
 pub fn get_net_info() []string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
 	mut info := []string
-	e_code, net_title := utils.parse(file_d, "Net_Info", "net_title")
-	_, net_description := utils.parse(file_d, "Net_Info", "net_description")
-	_, net_versoin := utils.parse(file_d, "Net_Info", "net_versoin")
-	_, net_port := utils.parse(file_d, "Net_Info", "net_port")
-	_, skrillec_key := utils.parse(file_d, "Net_Info", "skrillec_key")
+	net_title := utils.parse(file_d, "Net_Info", "net_title")
+	net_description := utils.parse(file_d, "Net_Info", "net_description")
+	net_versoin := utils.parse(file_d, "Net_Info", "net_versoin")
+	net_port := utils.parse(file_d, "Net_Info", "net_port")
+	skrillec_key := utils.parse(file_d, "Net_Info", "skrillec_key")
 	
 	info << net_title
 	info << net_description
@@ -23,10 +23,10 @@ pub fn get_net_info() []string {
 
 pub fn get_db_info() []string {
 	mut file_d := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
-	e_code, host := utils.parse(file_d, "MySQL", "host")
-	mut _, port := utils.parse(file_d, "MySQL", "port")
-	_, username := utils.parse(file_d, "MySQL", "username")
-	_, dbname := utils.parse(file_d, "MySQL", "dbname")
+	host := utils.parse(file_d, "MySQL", "host")
+	mut port := utils.parse(file_d, "MySQL", "port")
+	username := utils.parse(file_d, "MySQL", "username")
+	dbname := utils.parse(file_d, "MySQL", "dbname")
 
 	mut reply := []string
 	reply << host
