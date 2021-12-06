57e96db69d94aaec172222615a255c77bb38a6a5
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-06 13:50:34 -0500

    Update notifications.v

--- utils/notifications.v
+++ utils/notifications.v
@@ -20,12 +20,13 @@ pub fn (mut n NotificationSys) set_info(user string, ip string, cmd string, fmsg
 	n.notificate_msg = notificate_msg
 }
 
-pub fn (mut n NotificationSys) notificate() {
-	//get the current time
-	mut current_time := os.execute("date +'%m/%d/%y-%H:%m'").output
-	mut output := "[Action/CMD]: ${n.cmd}| [Timestamp]: ${current_time}\r\n"
-	output += "[User]: ${n.user} | [IP]: \r\n"
-	// output += "[Message]: ${s.File}"
+pub fn notificate(mut n NotificationSys) {
+	mut current_time := os.execute("date +'%m/%d/%y-%H:%m'").output // Current Time
+	mut output := "[Action/CMD]: ${n.cmd}| [Timestamp]: ${current_time}\r\n" // Outputing Action and Timestamp
+	output += "[User]: ${n.user} | [IP]: ${n.ip}\r\n"
+	if (n.fmsg).len > 0 {
+		output += "[Message]: ${n.fmsg}"
+	}
 	n.reset_info()
 }
 
