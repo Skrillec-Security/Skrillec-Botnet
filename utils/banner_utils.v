e33030ec9e361d432657b5ef26c4c2931fe8648c
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-09 14:28:11 -0500

    Update banner_utils.v

--- utils/banner_utils.v
+++ utils/banner_utils.v
@@ -14,7 +14,7 @@ pub fn file_reader(file string) string {
 	return os.read_file(os.getwd() + "/banners/${file}.txt") or {
 		""
 	}
-}
+}	
 
 pub fn output_ui(mut socket net.TcpConn, username string) {
 	utils.change_size(mut socket, 22, 106)
@@ -25,28 +25,49 @@ pub fn output_ui(mut socket net.TcpConn, username string) {
 	mut config_file := os.read_file(os.getwd() + "/assets/config.skrillec") or { "" }
 	socket.write_string(utils.replace_code(banner_file.trim_space(), username)) or { 0 }
 
-	// for g, i in banner_file.split("\n") {
-	// 	if i.starts_with("place_text(") { continue }
-	// 	if g == banner_file.split("\n").len-1 {
-	// 		socket.write_string("${i.trim_space()}") or { 0 }
-	// 	} else {
-	// 		socket.write_string("${i.trim_space()}\r\n") or { 0 }
-	// 	}
-	// }
-
 	for g in txt_file.split("\n") {
 		if g.starts_with("place_text") {
 			mut r, c := utils.get_str_between(g, "(", ")")
 			utils.place_text(mut socket, r.int(), c.int(), utils.replace_code(g.split("=")[1].trim_space(), username))
 		}
 	}
-	// utils.move_cursor(mut socket, )
 }
 
 pub fn output_login(mut socket net.TcpConn) {
 	utils.change_size(mut socket, 16, 61)
 	socket.write_string("\033[2J\033[1;1H") or { 0 }
 	socket.write_string(utils.replace_code(utils.file_reader("login"), "")) or { 0 }
+}
+
+pub fn output_banner(file string) {
+	mut banner_data := os.read_lines(os.getwd() + "/banners/${file}.txt") or { []string }
+
+	mut variables := []string
+	mut rgb_color := ""
+	mut rgb_colorr := ""
+
+	for i, line in banner_data {
+		if line.starts_with("var") {
+			if line.contains("=") {} else { print("[x] Error, Missing operator. '=', '+=', '-=' are valid!") }
+			if line.contains("[") && line.ends_with("]") {} else { println("[x] Error, Invalid syntax. Missing value! RGB, HEX or List Of Colors") }
+			if line.contains("var =") { println("[x] Error, Invalid syntax!") }
+			line_split := line.split(" ")
+			variables << line_split[1]
+			rgb_color = line_split[3]
+			rgb_colorr = line_split[5]
+
+			// println("${line_split[1]} | ${rgb_color} | ${rgb_colorr}")
+			continue
+		} else {
+			for var in variables {
+				if line.starts_with("{${var}}") {
+					banner_data[i] = utils.gradient(rgb_color.replace("[","").replace("]","").split(","), rgb_colorr.replace("[","").replace("]","").split(","), line.replace("{${var}}", "")) + "\x1b[39m"
+				}
+			}
+		}
+		
+		println(banner_data[i] + "\x1b[39m")
+	}
 }
 
 pub fn get_str_between(find_str string, start string, end string) (string, string) {
