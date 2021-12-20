829c90602f4f5ddb3f8368a69d67443de344ca8b
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-20 14:29:12 -0500

    Update user_crud.v

--- crud/user_crud.v
+++ crud/user_crud.v
@@ -4,6 +4,13 @@ import crud
 import mysql
 
 pub fn get_user(mut s mysql.Connection, username string) []string {
-	rows := crud.grab_user_info(mut s, username)
-	return rows
+	return crud.grab_user_info(mut s, username)
+}
+
+pub fn edit_info(mut s mysql.Connection, edits []string) int {
+	mut querys := []string
+	if "-ip" in edits { querys << "" }
+	if "-lvl" in edits { querys << "" }
+	if "-mtime" in edits { querys << "" }
+	if ""
 }
