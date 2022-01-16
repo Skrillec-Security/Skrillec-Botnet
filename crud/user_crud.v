9c1a235b6fb23a0a0d5fc81e4588003a5eb487e1
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-16 12:41:44 -0500

    Update user_crud.v

--- crud/user_crud.v
+++ crud/user_crud.v
@@ -7,10 +7,40 @@ pub fn get_user(mut s mysql.Connection, username string) []string {
 	return crud.grab_user_info(mut s, username)
 }
 
-pub fn edit_info(mut s mysql.Connection, edits []string) int {
-	mut querys := []string
-	if "-ip" in edits { querys << "" }
-	if "-lvl" in edits { querys << "" }
-	if "-mtime" in edits { querys << "" }
-	if ""
-}
+// pub fn edit_info(mut s mysql.Connection, edits []string) int {
+// 	mut querys := []string
+// 	for i, arg in  {
+// 		match arg {
+// 			"-ip" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "ip='${edits[i+1]}'")
+// 			}
+// 			if "-lvl" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "level='${edits[i+1]}'")
+// 			}
+// 			if "-mtime" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "maxtime='${edits[i+1]}'")
+// 			}
+// 			if "-conn" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "conn='${edits[i+1]}'")
+// 			}
+// 			if "-admin" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "admin='${edits[i+1]}'") 
+// 			}
+// 			if "expiry" in edits { 
+// 				crud.edit_user(mysql.Connection{
+
+// 				}, username, "expiry='${edits[i+1]}'")
+// 			}
+// 		}
+// 	}
+// }
