4d5884ebd92de0b130056ccef7e4297896e1cf9a
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 15:29:46 -0500

    Update api_crud.v

--- crud/api_crud.v
+++ crud/api_crud.v
@@ -1,7 +1,12 @@
 module crud
 
+import os
 import crud
 import mysql
+
+const (
+	local_api_path = "${os.getwd()}/assets/db/apis.skrillec"
+)
 
 pub fn get_api(mut s mysql.Connection, api_name string) []string {
 	return crud.grab_api_info(mut &s, api_name)
